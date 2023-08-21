bf17b395b4bd7b99986f18cfe238c8f07788be5e
Nam Lê <lenam07152002@gmail.com>
2023-08-21 22:49:13 +0700

    update url

--- Client/src/service/config.service.ts
+++ Client/src/service/config.service.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { animateScroll as scroll } from "react-scroll";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:8080/api/"
+  baseURL: "https://server-9izc.onrender.com/api/"
 })
 
 export const getAuthorizationHeaders = () => {
